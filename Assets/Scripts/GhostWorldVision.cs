6e15d1afb6ba4432d500f268907073f48cc37080
Oleksandr Moskalyk <kn191_mos@student.ztu.edu.ua>
2023-08-12 12:50:20 +0300

    Initial Commit

---
+++ Assets/Scripts/GhostWorldVision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostWorldVision : MonoBehaviour
+{
+    float maxGhostVision = 100f;
+    float ghostVision;
+
+    void Update()
+    {
+        
+    }
+}
