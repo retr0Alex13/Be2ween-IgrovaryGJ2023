db25256684cb860344cf52609fd84094a1114620
Oleksandr Moskalyk <kn191_mos@student.ztu.edu.ua>
2023-08-13 13:20:54 +0300

    Activate ghost vision only when player standing

--- Assets/Scripts/GhostVisionController.cs
+++ Assets/Scripts/GhostVisionController.cs
@@ -4,88 +4,19 @@ public class GhostVisionController : MonoBehaviour
 {
     Camera playerCamera;
 
-    float maxGhostVision = 100f;
-    float ghostVision;
-
-    [SerializeField]
-    float ghostVisionReduceRate = 10f;
-
-    [SerializeField]
-    float ghostVisionRecoveryDelay = 2f;
-
-    [SerializeField]
-    float ghostVisionRecoveryRate = 5f;
-
-    float timer;
-
-    bool ghostVisionStatus;
-
     void Awake()
     {
         playerCamera = GetComponent<Camera>();
     }
 
-    void Start()
-    {
-        ghostVision = maxGhostVision;
-    }
-
     void Update()
     {
-        ReduceVisionPoints();
-        HandleVisionRecharge();
+
     }
 
-    private void HandleVisionRecharge()
+    public void SwitchGhostVision(bool status)
     {
-        if (!ghostVisionStatus)
-        {
-            timer += Time.deltaTime;
-
-            if (timer > ghostVisionRecoveryDelay)
-            {
-                timer = ghostVisionRecoveryDelay;
-                VisionRecharge();
-            }
-        }
-        else
-        {
-            timer = 0;
-            return;
-        }
-    }
-
-    private void VisionRecharge()
-    {
-        ghostVision += Time.deltaTime * ghostVisionRecoveryRate;
-
-        if (ghostVision > maxGhostVision)
-        {
-            ghostVision = maxGhostVision;
-        }
-    }
-
-    private void ReduceVisionPoints()
-    {
-        if (ghostVisionStatus)
-        {
-            ghostVision -= ghostVisionReduceRate * Time.deltaTime;
-
-            if (ghostVision < 0)
-            {
-                ghostVision = 0;
-                ghostVisionStatus = false;
-
-                SwitchGhostLayer(ghostVisionStatus);
-            }
-            Debug.Log($"Ghost vision: {ghostVision}");
-        }
-    }
-
-    public void SwitchVision(bool visionStatus)
-    {
-        ghostVisionStatus = visionStatus;
-        SwitchGhostLayer(ghostVisionStatus);
+        SwitchGhostLayer(status);
     }
 
     void SwitchGhostLayer(bool visionStatus)
